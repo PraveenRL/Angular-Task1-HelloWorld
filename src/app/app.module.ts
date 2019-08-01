0d2233c472299bdcd3ed1ea7f5aafc2a7e6ceed0
PraveenRL <rlppraveen@gmail.com>
2019-08-01 15:10:21 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorldComponent } from './world/world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorldComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/world/world.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-world',
+  templateUrl: './world.component.html',
+  styleUrls: ['./world.component.css']
+})
+export class WorldComponent implements OnInit {
+
+  constructor() { }
+
+  name = 'Praveen';
+
+  ngOnInit() {
+  }
+
+}
